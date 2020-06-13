bf639aa9693c874e7c3b5812a75e74919d97fab8
Soumen Khara <Soumen2015.s.k@gmail.com>
2020-06-13 07:35:07 +0530

    Add project files.

---
+++ bass dll example/Form1.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TrackBar5 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar4 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar3 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar2 = New System.Windows.Forms.TrackBar()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        CType(Me.TrackBar5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Column1"
+        Me.Column1.Name = "Column1"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(228, 173)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(20, 13)
+        Me.Label5.TabIndex = 23
+        Me.Label5.Text = "8K"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(176, 173)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(20, 13)
+        Me.Label4.TabIndex = 22
+        Me.Label4.Text = "4K"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(125, 173)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(20, 13)
+        Me.Label3.TabIndex = 21
+        Me.Label3.Text = "1K"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(71, 173)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(25, 13)
+        Me.Label2.TabIndex = 20
+        Me.Label2.Text = "500"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 173)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(25, 13)
+        Me.Label1.TabIndex = 19
+        Me.Label1.Text = "100"
+        '
+        'TrackBar5
+        '
+        Me.TrackBar5.Location = New System.Drawing.Point(216, 32)
+        Me.TrackBar5.Maximum = 15
+        Me.TrackBar5.Minimum = -15
+        Me.TrackBar5.Name = "TrackBar5"
+        Me.TrackBar5.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.TrackBar5.Size = New System.Drawing.Size(45, 138)
+        Me.TrackBar5.TabIndex = 18
+        Me.TrackBar5.TickStyle = System.Windows.Forms.TickStyle.Both
+        '
+        'TrackBar4
+        '
+        Me.TrackBar4.Location = New System.Drawing.Point(165, 32)
+        Me.TrackBar4.Maximum = 15
+        Me.TrackBar4.Minimum = -15
+        Me.TrackBar4.Name = "TrackBar4"
+        Me.TrackBar4.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.TrackBar4.Size = New System.Drawing.Size(45, 138)
+        Me.TrackBar4.TabIndex = 17
+        Me.TrackBar4.TickStyle = System.Windows.Forms.TickStyle.Both
+        '
+        'TrackBar3
+        '
+        Me.TrackBar3.Location = New System.Drawing.Point(114, 32)
+        Me.TrackBar3.Maximum = 15
+        Me.TrackBar3.Minimum = -15
+        Me.TrackBar3.Name = "TrackBar3"
+        Me.TrackBar3.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.TrackBar3.Size = New System.Drawing.Size(45, 138)
+        Me.TrackBar3.TabIndex = 16
+        Me.TrackBar3.TickStyle = System.Windows.Forms.TickStyle.Both
+        '
+        'TrackBar2
+        '
+        Me.TrackBar2.Location = New System.Drawing.Point(63, 32)
+        Me.TrackBar2.Maximum = 15
+        Me.TrackBar2.Minimum = -15
+        Me.TrackBar2.Name = "TrackBar2"
+        Me.TrackBar2.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.TrackBar2.Size = New System.Drawing.Size(45, 138)
+        Me.TrackBar2.TabIndex = 15
+        Me.TrackBar2.TickStyle = System.Windows.Forms.TickStyle.Both
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(132, 9)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox1.TabIndex = 14
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(12, 32)
+        Me.TrackBar1.Maximum = 15
+        Me.TrackBar1.Minimum = -15
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.TrackBar1.Size = New System.Drawing.Size(45, 138)
+        Me.TrackBar1.TabIndex = 13
+        Me.TrackBar1.TickStyle = System.Windows.Forms.TickStyle.Both
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(103, 228)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(276, 270)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TrackBar5)
+        Me.Controls.Add(Me.TrackBar4)
+        Me.Controls.Add(Me.TrackBar3)
+        Me.Controls.Add(Me.TrackBar2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.TrackBar5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TrackBar5 As TrackBar
+    Friend WithEvents TrackBar4 As TrackBar
+    Friend WithEvents TrackBar3 As TrackBar
+    Friend WithEvents TrackBar2 As TrackBar
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents Button1 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ bass dll example/Form1.vb
@@ -0,0 +1,73 @@
+﻿Imports Un4seen.Bass
+Public Class Form1
+    Public par As New BASS_DX8_PARAMEQ
+    Public fx As Integer() = New Integer(4 - 0) {}
+    Public chan As Integer
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_DEFAULT, Me.Handle)
+        Un4seen.Bass.BassNet.Registration("sdsadasd@gmail.com", "b88d1754d700e49a")
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        OpenFileDialog1.Filter = "MediaFiles(*.wav;*.mp3)|*.wav;*.mp3"
+        If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            If CBool(chan) Then Bass.BASS_StreamFree(chan)
+            chan = Bass.BASS_StreamCreateFile(OpenFileDialog1.FileName, 0, 0, BASSFlag.BASS_DEFAULT)
+            CheckBox1_CheckedChanged(sender, e)
+            Bass.BASS_ChannelPlay(chan, False)
+        End If
+    End Sub
+
+    Private Sub TrackBar1_Scroll(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TrackBar1.Scroll
+        BASS_SetParametersEQ(fx(0), 100, TrackBar1.Value)
+    End Sub
+
+    Private Sub TrackBar2_Scroll(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TrackBar2.Scroll
+        BASS_SetParametersEQ(fx(1), 500, TrackBar2.Value)
+    End Sub
+
+    Private Sub TrackBar3_Scroll(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TrackBar3.Scroll
+        BASS_SetParametersEQ(fx(2), 1000, TrackBar3.Value)
+    End Sub
+
+    Private Sub TrackBar4_Scroll(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TrackBar4.Scroll
+        BASS_SetParametersEQ(fx(3), 4000, TrackBar4.Value)
+    End Sub
+
+    Private Sub TrackBar5_Scroll(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TrackBar5.Scroll
+        BASS_SetParametersEQ(fx(4), 8000, TrackBar5.Value)
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            For i As Integer = 0 To 4
+                fx(i) = Bass.BASS_ChannelSetFX(chan, BASSFXType.BASS_FX_DX8_PARAMEQ, 0)
+            Next i
+            BASS_UpdateParamsEQ(sender, e)
+        Else
+            For x As Integer = 0 To 4
+                Bass.BASS_ChannelRemoveFX(chan, fx(x))
+            Next x
+        End If
+    End Sub
+
+    Public Function BASS_SetParametersEQ(ByVal fx As Integer, ByVal center As Integer, ByVal gain As Integer) As Boolean
+        par.fBandwidth = 18.0!
+        par.fCenter = CSng(center)
+        par.fGain = CSng(gain)
+        Return Bass.BASS_FXSetParameters(fx, par)
+    End Function
+
+    Public Sub BASS_UpdateParamsEQ(ByVal sender As Object, ByVal e As EventArgs)
+        TrackBar1_Scroll(sender, e)
+        TrackBar2_Scroll(sender, e)
+        TrackBar3_Scroll(sender, e)
+        TrackBar4_Scroll(sender, e)
+        TrackBar5_Scroll(sender, e)
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        Bass.BASS_Free()
+    End Sub
+End Class
